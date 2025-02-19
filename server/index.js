d68887137442f0bb332d39e8bb6a0193ff9d5f70
Komal <komal.professional.work@gmail.com>
2025-02-20 03:24:21 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ import BlogLikeRoute from './routes/Bloglike.route.js';
 
 dotenv.config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000
 const app = express()
 
 app.use(cookieParser())
